7ca5c4be82b7aedbc58c31da017d176548a1fa88
Jim Newton <jimka2001@users.noreply.github.com>
2025-05-14 16:16:59 +0200

    adding example

--- clojure/examples/els2025.clj
+++ clojure/examples/els2025.clj
@@ -22,3 +22,37 @@
              c (range b 100)]
   (= (+ (* a a) (* b b))
      (* c c)))
+
+(def words 
+  (with-open [rdr (clojure.java.io/reader "/usr/share/dict/words")]
+    (doall (line-seq rdr))))
+
+
+(defn alpha-< [a b]
+  (cond (= a b)
+        false
+
+        (empty? a)
+        true
+
+        (empty? b)
+        false
+
+        
+
+        (not= (first a) (first b))
+        (neg? (compare (first a) (first b)))
+
+        :else
+        (alpha-< (rest a) (rest b))))
+
+
+(alpha-< '(a) '(b a))
+
+
+(hb/+forall [a words
+             b words
+             c words]
+  (hb/+implies (and (alpha-< a b)
+                    (alpha-< b c))
+               (alpha-< a c)))
